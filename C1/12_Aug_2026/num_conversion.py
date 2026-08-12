ca3e586c930db71f9da83bc363a18ea2779a804b
amit26717-cmd <patelamit26717@gmail.com>
2026-08-12 12:27:08 +0530

    n

---
+++ C1/12_Aug_2026/num_conversion.py
@@ -0,0 +1,41 @@
+s1= 'Amit' \
+'kumar' \
+'patel'
+print(s1)
+
+s2 = "M.sc." \
+"h"
+s3 = '''amit'''
+
+s4 = '''this is character'''
+print(s4[5:7])
+b =" this is our da class"
+l ='j' \
+'k' \
+'i' \
+'l'
+print(l)
+i = 'k'
+'k'
+print(i)
+len(b)   
+>>>print("kkkk")
+print(float(10))
+print(float(True))
+print(float("10"))
+print(float("ten"))
+print(float(10+5j))
+a="fgg"
+print(type(a))
+print(complex(10))
+print(complex(10.5))
+print(complex(True))
+print(complex(False))
+print(complex("200"))
+print(complex("ten"))
+
+print(bool(0))
+print(bool(10000))
+print(bool(10.4524))
+print(bool("45hg"))
+print(bool(122))
