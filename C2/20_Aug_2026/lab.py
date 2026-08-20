960349ae5907b4e3e48898d4851a9b812902fc6e
amit26717-cmd <patelamit26717@gmail.com>
2026-08-20 12:13:19 +0530

    text

---
+++ C2/20_Aug_2026/lab.py
@@ -0,0 +1,23 @@
+#sentence input 
+name = eval(input("Enter List :- "))
+#PRINT SENTENCE 
+print(name)
+
+#convert into lower case 
+low = name.lower()
+print(low)
+
+#split the word 
+spli=name.split()
+print(spli)
+
+#count totaal word 
+count = len(spli)
+print("Number of words : ",count)
+
+
+
+#find word in the sentence 
+print('kumar' in low)
+
+
