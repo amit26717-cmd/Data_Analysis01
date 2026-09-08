16cbe276609a0df3e28eeeff2005a3a9a3b9d008
amit26717-cmd <patelamit26717@gmail.com>
2026-09-08 12:21:29 +0530

    Use relstional operator to find data in the data sheet '

---
+++ C2/08_Sep_2026/panda.py
@@ -0,0 +1,15 @@
+import pandas as pd 
+df=pd.read_csv('orders.csv')
+df.head()
+# # print(df)
+# df.info()
+# # df.columns[2]
+# # print(df['Class'])
+# df = pd.read_csv('t.csv', sep=';')
+# print(df)
+
+# print(df.loc[15])
+# print(df.iloc[0,'word_freq_all'])
+
+pr=df[(df['Category']=="Electronics")&(df["Country"]=="Japan")&(df["Product"]=="Mouse")]
+print(pr)
